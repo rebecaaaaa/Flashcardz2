472a59ee62f9854d211d1e67823e8a42785f3e24
Rebeca Chavez <rebecachavez@rebecas-mbp>
2018-10-13 14:23:43 -0700

    Initial Commit

---
+++ Flashcardz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashcardz
+//
+//  Created by Rebeca Chavez on 10/13/18.
+//  Copyright © 2018 Rebeca Chavez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
